f79401e8547d662174ebdcf4c3dbb8d73bb98946
fbtngus2-coder <fbtngus2@gmail.com>
2025-12-19 13:25:12 +0900

    폴더 정리

--- fix_rls.sql
+++
@@ -1,3 +0,0 @@
--- Allow anonymous inserts for setup purposes (CAUTION: For dev only)
-create policy "Enable insert for anon" on marketing_calendar for insert with check (true);
-create policy "Enable all access for anon" on input_data for all using (true) with check (true);
